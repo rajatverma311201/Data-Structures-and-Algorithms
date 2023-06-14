7819faf364ec3efa51b3ac61eeec07e32d6eb9ee
Rajat Verma <rajat311201@gmail.com>
2023-06-14 12:22:22 +0530

    Update TopologicalSortDFS.java

--- Graph/TopologicalSortDFS.java
+++ Graph/TopologicalSortDFS.java
@@ -4,6 +4,14 @@ import java.util.Stack;
 
 public class TopologicalSortDFS {
 
+    /**
+     * Function to perform dfs on a graph
+     * 
+     * @param ind current node on which dfs called
+     * @param adj adjacency list of graph
+     * @param vis visited array for graph
+     * @param st  stack to store the topo sort
+     */
     public void dfs(int ind, List<Integer>[] adj, boolean[] vis, Stack<Integer> st) {
         vis[ind] = true;
         for (int node : adj[ind]) {
@@ -14,6 +22,14 @@ public class TopologicalSortDFS {
         st.push(ind);
     }
 
+    /**
+     * Function to find the topoSort, it calls the dfs function
+     * 
+     * @param edges    array of edges of graph in the form {u, v} indicating edge
+     *                 from u to v (u->v)
+     * @param vertices number of vertices for 0 based graph
+     * @return array containing topological sort of the Directed Acyclic Graph
+     */
     @SuppressWarnings("unchecked")
     public int[] topoSort(int[][] edges, int vertices) {
 
