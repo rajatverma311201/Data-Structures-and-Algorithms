a6d1e7f9023c567d2dc9a3d9d49475fa0ad815e0
Rajat Verma <rajat311201@gmail.com>
2023-06-16 14:02:37 +0530

    Add files via upload

---
+++ Graph/GraphUtils.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class GraphUtils {
+
+    @SuppressWarnings("unchecked")
+    public static List<Integer>[] createAdjacencyList(int[][] edges, int vertices, boolean isDirected) {
+        List<Integer>[] adj = new List[vertices];
+        for (int i = 0; i < vertices; i++)
+            adj[i] = new ArrayList<>();
+
+        for (int[] edge : edges) {
+            adj[edge[0]].add(edge[1]);
+            if (!isDirected) {
+                adj[edge[1]].add(edge[0]);
+            }
+        }
+
+        return adj;
+    }
+
+    public static int[] convertListToArray(List<Integer> li) {
+        int[] ansArray = new int[li.size()];
+        for (int i = 0; i < li.size(); i++) {
+            ansArray[i] = li.get(i);
+        }
+        return ansArray;
+    }
+}
