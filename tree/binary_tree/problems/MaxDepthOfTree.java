d45eddcf54e61d84abf71a071382d7551cdfca39
Rajat Verma <rajat311201@gmail.com>
2023-06-20 00:10:45 +0530

    max depth

--- tree/binary_tree/problems/MaxDepthOfTree.java
+++ tree/binary_tree/problems/MaxDepthOfTree.java
@@ -1,5 +1,15 @@
 package tree.binary_tree.problems;
 
+import tree.TreeNode;
+
 public class MaxDepthOfTree {
+
+    public int maxDepth(TreeNode root) {
+        if (root == null)
+            return 0;
+        int left = maxDepth(root.left);
+        int right = maxDepth(root.right);
+        return Math.max(left, right) + 1;
+    }
 
 }
