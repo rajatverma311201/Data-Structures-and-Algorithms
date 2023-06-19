e4a348f51b3c0690adc17ea52e1045fcd49099e4
Rajat Verma <rajat311201@gmail.com>
2023-06-19 05:46:15 +0000

    postorder

---
+++ tree/binary_tree/traversal/PostOrderTraversal.java
@@ -0,0 +1,27 @@
+package tree.binary_tree.traversal;
+
+import tree.binary_tree.TreeNode;
+
+public class PostOrderTraversal {
+
+    private void postorder(TreeNode root, List<Integer> ans) {
+        if (root == null) {
+            return;
+        }
+        postorder(root.left, ans);
+        postorder(root.right, ans);
+        ans.add(root.data);
+    }
+
+    public List<Integer> getTraversal(TreeNode root) {
+        List<Integer> ans = new ArrayList<>();
+
+        if (root == null) {
+            return ans;
+        }
+
+        postorder(root, ans);
+        return ans;
+    }
+
+}
