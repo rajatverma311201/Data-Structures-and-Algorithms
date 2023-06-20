e88948f2e425d1b016744e62de20d6c8065a35f9
Rajat Verma <rajat311201@gmail.com>
2023-06-20 21:47:32 +0530

    added BalancedTree

--- tree/binary_tree/problems/BalancedTree.java
+++ tree/binary_tree/problems/BalancedTree.java
@@ -1,5 +1,26 @@
 package tree.binary_tree.problems;
 
+import tree.TreeNode;
+
 public class BalancedTree {
+
+    public boolean isBalanced(TreeNode root) {
+        return checkBalanced(root) != -1;
+    }
+
+    private int checkBalanced(TreeNode root) {
+        if (root == null)
+            return 0;
+        int left = checkBalanced(root.left);
+        int right = checkBalanced(root.right);
+
+        if (left == -1 || right == -1)
+            return -1;
+
+        if (Math.abs(left - right) > 1)
+            return -1;
+
+        return Math.max(left, right) + 1;
+    }
 
 }
