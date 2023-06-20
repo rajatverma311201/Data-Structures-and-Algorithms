1641e5e3eac47c662eeef5d3f25e2e150c13462e
Rajat Verma <rajat311201@gmail.com>
2023-06-20 23:44:38 +0530

    identical trees

--- tree/binary_tree/problems/IdenticalTrees.java
+++ tree/binary_tree/problems/IdenticalTrees.java
@@ -1,5 +1,23 @@
 package tree.binary_tree.problems;
 
+import tree.TreeNode;
+
 public class IdenticalTrees {
+
+    boolean areIdentical(TreeNode root1, TreeNode root2) {
+
+        if (root1 == null || root2 == null) {
+            return root1 == root2;
+        }
+
+        if (root1.data != root2.data) {
+            return false;
+        }
+
+        boolean leftIdentity = areIdentical(root1.left, root2.left);
+        boolean rightIdentity = areIdentical(root1.right, root2.right);
+
+        return leftIdentity && rightIdentity;
+    }
 
 }
