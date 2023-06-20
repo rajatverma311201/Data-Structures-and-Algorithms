87174a540865fae5b4131eb4081abfba70dfc085
Rajat Verma <rajat311201@gmail.com>
2023-06-21 00:17:01 +0530

    root to node path

--- tree/binary_tree/problems/RootToNodePathInTree.java
+++ tree/binary_tree/problems/RootToNodePathInTree.java
@@ -1,5 +1,44 @@
 package tree.binary_tree.problems;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import tree.TreeNode;
+
 public class RootToNodePathInTree {
+
+    private boolean findPathList(TreeNode root, int x, List<Integer> ans) {
+
+        if (root == null) {
+            return false;
+        }
+
+        ans.add(root.data);
+
+        if (root.data == x) {
+            return true;
+        }
+
+        boolean goLeft = findPathList(root.left, x, ans);
+        boolean goRight = findPathList(root.right, x, ans);
+
+        if (goLeft || goRight) {
+            return true;
+        }
+
+        ans.remove(ans.size() - 1);
+
+        return false;
+
+    }
+
+    public List<Integer> getPath(TreeNode root, int x) {
+
+        List<Integer> path = new ArrayList<>();
+
+        findPathList(root, x, path);
+
+        return path;
+    }
 
 }
