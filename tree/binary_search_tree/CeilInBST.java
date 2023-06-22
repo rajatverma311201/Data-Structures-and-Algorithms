8c41f2d50e9273a8c130e4f8c85da803f7361d15
Rajat Verma <rajat311201@gmail.com>
2023-06-22 23:44:37 +0530

    ceil bst

--- tree/binary_search_tree/CeilInBST.java
+++ tree/binary_search_tree/CeilInBST.java
@@ -1,5 +1,28 @@
 package tree.binary_search_tree;
 
+import tree.TreeNode;
+
 public class CeilInBST {
+
+    public int getCeil(TreeNode root, int key) {
+
+        int ans = -1;
+
+        while (root != null) {
+            if (root.data == key) {
+                ans = root.data;
+                break;
+            }
+
+            if (key > root.data) {
+                root = root.right;
+            } else {
+                ans = root.data;
+                root = root.left;
+            }
+        }
+
+        return ans;
+    }
 
 }
--- tree/binary_search_tree/CheckIfValidBST.java
+++ tree/binary_search_tree/CheckIfValidBST.java
@@ -1,5 +1,15 @@
 package tree.binary_search_tree;
 
+import tree.TreeNode;
+
 public class CheckIfValidBST {
+
+    public boolean isValidBST(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+
+        return true;
+    }
 
 }
