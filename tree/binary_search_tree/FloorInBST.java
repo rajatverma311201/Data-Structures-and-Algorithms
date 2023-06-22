606e09c2ace59de8f4674a0e21ba2478508065f6
Rajat Verma <rajat311201@gmail.com>
2023-06-22 23:47:07 +0530

    floor inn bst

--- tree/binary_search_tree/FloorInBST.java
+++ tree/binary_search_tree/FloorInBST.java
@@ -1,5 +1,29 @@
 package tree.binary_search_tree;
 
+import tree.TreeNode;
+
 public class FloorInBST {
+
+    public int getFloor(TreeNode root, int key) {
+        int ans = -1;
+
+        while (root != null) {
+            if (root.data == key) {
+                ans = root.data;
+                break;
+            }
+
+            if (key < root.data) {
+                root = root.left;
+            }
+
+            else if (key > root.data) {
+                ans = root.data;
+                root = root.right;
+            }
+        }
+
+        return ans;
+    }
 
 }
