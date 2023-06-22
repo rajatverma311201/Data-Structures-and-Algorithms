f4b35a2226d27e80afbfa65d45aadd45b9009c21
Rajat Verma <rajat311201@gmail.com>
2023-06-22 22:15:40 +0530

    search node in bst

--- tree/binary_search_tree/SearchANodeInBST.java
+++ tree/binary_search_tree/SearchANodeInBST.java
@@ -1,5 +1,25 @@
 package tree.binary_search_tree;
 
+import tree.TreeNode;
+
 public class SearchANodeInBST {
+
+    public boolean search(TreeNode root, int key) {
+
+        if (root == null) {
+            return false;
+        }
+
+        if (root.data == key) {
+            return true;
+        }
+
+        if (key > root.data) {
+            return search(root.right, key);
+        } else {
+            return search(root.left, key);
+        }
+
+    }
 
 }
