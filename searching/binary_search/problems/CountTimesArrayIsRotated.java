6eb2b31c36b3ef40a9ad53e2e936b096b96d3ec9
Rajat Verma <rajat311201@gmail.com>
2023-06-27 23:29:19 +0530

    count times array is rotated

--- searching/binary_search/problems/CountTimesArrayIsRotated.java
+++ searching/binary_search/problems/CountTimesArrayIsRotated.java
@@ -1,13 +1,20 @@
 package searching.binary_search.problems;
 
-public class MinimumInRotatedSortedArray {
+public class CountTimesArrayIsRotated {
+
+    public int countTimes(int[] nums) {
+        int idx = findMinimum(nums);
+
+        return idx;
+    }
 
     // UNIQUE ELEMENTS
+    // returns minimum element index
     public int findMinimum(int[] nums) {
         int low = 0;
         int high = nums.length - 1;
 
-        int ans = Integer.MAX_VALUE;
+        int ans = 0;
 
         while (low <= high) {
 
@@ -17,17 +24,20 @@ public class MinimumInRotatedSortedArray {
             if (nums[low] <= nums[high])
 
             {
-                ans = Math.min(ans, nums[low]);
+                // ans = Math.min(ans, nums[low]);
+                ans = nums[ans] < nums[low] ? ans : low;
                 break;
             }
 
             int mid = low + (high - low) / 2;
 
             if (nums[low] <= nums[mid]) {
-                ans = Math.min(ans, nums[low]);
+                // ans = Math.min(ans, nums[low]);
+                ans = nums[ans] < nums[low] ? ans : low;
                 low = mid + 1;
             } else {
-                ans = Math.min(ans, nums[mid]);
+                // ans = Math.min(ans, nums[mid]);
+                ans = nums[ans] < nums[mid] ? ans : mid;
                 high = mid - 1;
             }
         }
