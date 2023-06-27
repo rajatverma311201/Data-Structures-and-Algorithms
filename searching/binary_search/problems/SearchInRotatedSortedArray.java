07845e6d5dd58cbc9154af89e1282b2bbb74648e
Rajat Verma <rajat311201@gmail.com>
2023-06-27 23:02:00 +0530

    rotated sorted array search 2

--- searching/binary_search/problems/SearchInRotatedSortedArray.java
+++ searching/binary_search/problems/SearchInRotatedSortedArray.java
@@ -8,7 +8,7 @@ public class SearchInRotatedSortedArray {
      * 
      * @param nums
      * @param target
-     * @return
+     * @return int - index of element if present and -1 otherwise
      */
     public int searchType1(int[] nums, int target) {
 
@@ -48,13 +48,48 @@ public class SearchInRotatedSortedArray {
      * 
      * ELEMENTS ARE NOT UNIQUE
      * ELEMENTS ARE REPEATING
+     * DUPLICATE ELEMENTS
      * 
      * @param nums
      * @param target
-     * @return
+     * @return boolean - if target is present or not
      */
-    public int searchType2(int[] nums, int target) {
-        return -1;
+    public boolean searchType2(int[] nums, int target) {
+
+        int low = 0;
+        int high = nums.length - 1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] == target) {
+                return true;
+            }
+            if (nums[low] == nums[mid] && nums[mid] == nums[high]) {
+                low++;
+                high--;
+                continue;
+            }
+
+            // LEFT HALF SORTED
+            if (nums[low] <= nums[mid]) {
+                if (nums[low] <= target && target <= nums[mid]) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            }
+            // RIGHT HALF SORTED
+            else {
+                if (nums[mid] <= target && target <= nums[high]) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
+            }
+        }
+
+        return false;
     }
 
 }
